d68811de560704a2edd5d9c6792a2869a1ce739f
Aelia Abbas <aelia.abbas@cityrdbclab5015.auckland.natcoll.ac.nz>
2015-10-07 15:47:15 +1300

    first

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+// // Hides the paragaphs when clicked, hides fast
+// $(document).ready(function(){
+// 	$("p").click(function(){
+// 		$(this).hide("fast");
+// 		// $(this).hide("slow");
+// 	});
+// });
+
+// // Hides the button when clicked
+// $(document).ready(function(){
+// 	$("button").click(function(){
+// 		$(this).hide("slow");
+// 	});
+// });
+
+// // Shows the button when the link is clicked
+// $(document).ready(function(){
+// 	$("button").hide();
+// 	$("#login").click(function(){
+// 		$("button").show("slow");
+// 	});
+// });
+
+// // Shows n hides the button when the link is clicked
+// $(document).ready(function(){
+// 	$("#login").click(function(){
+// 		$("button").toggle("slow");
+// 	});
+// });
+
+
+// Shows n hides the navigation when the link is clicked
+$(document).ready(function(){
+	$("#home").click(function(){
+		$("nav").toggle("slow");
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
